7b853efbcdb115c1af8c350ad0334801421cd938
Diego Z <diego.zegarra78@gmail.com>
2020-03-18 00:47:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-function shout(string){
-  return string.toUpperCase() 
+function shout(string) {
+  return string.toUpperCase()
 }
-function whisper(string){
+
+function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string){
-  console.log('HELLO')
-}
-function logWhisper(string){
-  console.log('hello')
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
-
-
-function sayHiToGrandma(string){
- if (string === string.toLowerCase()){
-  return "I can't hear you!" 
- }
- if (string === string.toUpperCase()){
-   return "YES INDEED!"
- }
- if (string === "I love you, Grandma."){
-   return "I love you, too."
- }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return ("I can't hear you!")
+  }
+  if (string === string.toUpperCase()) {
+    return ("YES INDEED!")
+  }
+  if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
+  
+  
+  
+  
+  
+  
+}
